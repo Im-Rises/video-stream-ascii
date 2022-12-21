7c15ac329d2059223e7d5aec168f053a733ae22d
Im-Rises <quentin-morel88@hotmail.com>
2022-12-20 20:38:14 -0500

    Added all libs

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'xo',
+	],
+	overrides: [
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+	],
+	rules: {
+		'eol-last': [
+			'error',
+			'always',
+		],
+	},
+};
