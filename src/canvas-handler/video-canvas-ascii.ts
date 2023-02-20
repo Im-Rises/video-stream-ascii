8d52c45f75ee57896b1c0b910a2e226c42a9bf4c
Im-Rises <quentin-morel88@hotmail.com>
2023-02-20 01:13:38 -0500

    wip

--- src/canvas-handler/video-canvas-ascii.ts
+++ src/canvas-handler/video-canvas-ascii.ts
@@ -20,34 +20,62 @@ const getAsciiFromImage = (imageData: ImageData, asciiChars: string) => {
 };
 
 const calculateAndSetFontSize = (pretag: HTMLPreElement, asciiTextWidth: number, asciiTextHeight: number, screenWidth: number, screenHeight: number) => {
-	const canvas = document.createElement('canvas');
-	const ctx = canvas.getContext('2d');
-
-	let fontSize = 1;
-	ctx!.font = `${fontSize}px monospace`;
-
-	const textToMeasureWidth = String(' ').repeat(asciiTextWidth);
-
-	while (ctx!.measureText(textToMeasureWidth).width < screenWidth) {
-		ctx!.font = `${fontSize}px monospace`;
-		fontSize += 1;
-	}
-
-	// const textToMeasureHeight = String('\n').repeat(asciiTextHeight);
-	if (fontSize * asciiTextHeight > screenHeight) {
-		while (fontSize * asciiTextHeight > screenHeight) {
-			fontSize -= 1;
-			ctx!.font = `${fontSize}px monospace`;
+	// Create a text fill with the same width as the ascii text and add \n for the end of each line
+	let stringToMeasure = 'W';
+	for (let i = 0; i < asciiTextHeight - 1; i++) {
+		for (let j = 0; j < asciiTextWidth - 1; j++) {
+			stringToMeasure += 'W';
 		}
+
+		stringToMeasure += '\n';
 	}
 
-	if (fontSize < 1) {
-		fontSize = 1;
+	// Split the string into lines
+	const lines = stringToMeasure.split('\n');
+	// console.log(lines[0].length);
+	// console.log(lines.length);
+
+	const canvas = document.createElement('canvas');
+	const context = canvas.getContext('2d');
+	let fontSize = 1;
+	context!.font = `${fontSize}px monospace`;
+
+	while (context!.measureText(lines[0]).width < screenWidth && lines.length * fontSize < screenHeight) {
+		fontSize += 1;
+		context!.font = `${fontSize}px monospace`;
 	}
+
+	fontSize -= 1;
 
 	pretag.style.fontSize = `${fontSize}px`;
-
-	console.log('fontSize', fontSize);
+	console.log(`Setting font size to ${fontSize}`);
 };
+
+/*
+* 	const fontSizeWidth = Math.floor(screenWidth / asciiTextWidth);
+	const fontSizeHeight = Math.floor(screenHeight / asciiTextHeight);
+	const fontSizeMin = Math.min(fontSizeWidth, fontSizeHeight);
+	const fontSizeMax = Math.max(fontSizeWidth, fontSizeHeight);
+
+	// console.log('fontSizeMax * asciiTextWidth > screenWidth', fontSizeMax * asciiTextWidth > screenWidth);
+	// console.log('fontSizeMax = ', fontSizeMax);
+	// console.log('asciiTextWidth = ', asciiTextWidth);
+	// console.log('screenWidth = ', screenWidth);
+	//
+	// console.log('fontSizeMax * asciiTextHeight > screenHeight', fontSizeMax * asciiTextHeight > screenHeight);
+	// console.log('fontSizeMax = ', fontSizeMax);
+	// console.log('asciiTextHeight = ', asciiTextHeight);
+	// console.log('screenHeight = ', screenHeight);
+	if (fontSizeMax * asciiTextWidth > screenWidth) {
+		pretag.style.fontSize = `${fontSizeMin}px`;
+	} else {
+		pretag.style.fontSize = `${fontSizeMax}px`;
+	}
+
+	console.log('fontSizeMin = ', fontSizeMin);
+	console.log('fontSizeMax = ', fontSizeMax);
+
+	// pretag.style.fontSize = `${fontsize}px`;
+* */
 
 export {getAsciiFromImage, calculateAndSetFontSize};
