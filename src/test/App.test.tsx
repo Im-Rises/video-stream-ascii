12a086156be51c5971fc05bb99cb682de91c5878
Im-Rises <quentin-morel88@hotmail.com>
2023-04-10 01:06:33 -0400

    wip

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, screen} from '@testing-library/react';
-import {GITHUB_LINK_TEXT} from '../constants/pixel-ascii';
 import App from '../App';
+
 test('Render camera-ascii', () => {
 	// render(<App/>);
 	// const videoAscii = screen.getByTestId('camera-ascii-test');
