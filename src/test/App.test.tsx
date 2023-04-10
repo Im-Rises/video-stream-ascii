7bdf90eff0cfb60587abfd883d294f9140544682
Im-Rises <quentin-morel88@hotmail.com>
2023-04-10 00:27:15 -0400

    wip

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import {GITHUB_LINK_TEXT} from '../constants/pixel-ascii';
 import App from '../App';
-
-test('Render GitHub Project Panel', () => {
-	// render(<App/>);
-	// const linkElement = screen.getByText(GITHUB_LINK_TEXT);
-	// expect(linkElement).toBeInTheDocument();
-});
-
 test('Render camera-ascii', () => {
 	// render(<App/>);
 	// const videoAscii = screen.getByTestId('camera-ascii-test');
